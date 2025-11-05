a1ebc502f8ba1476a40ac2cd9e5ed337e706c1ae
Chantell Griffin <shesurecodes@gmail.com>
2025-11-05 04:08:02 +0000

    Added a repo and files to follow with the intermediate python course

---
+++ section_1/matplotlib/Matplotlib.py
@@ -0,0 +1,5 @@
+# Matplotlib
+# Data visualization is a key skill for aspiring data scientists. 
+# Matplotlib makes it easy to create meaningful and insightful plots. 
+# In this chapter, you’ll learn how to build various types of plots, and customize them 
+# to be more visually appealing and interpretable.
